d2b8bce3b295adc49ea3dee44c034e8268011ec3
Weirong Xu <weirongxu.raidou@gmail.com>
2018-05-22 01:49:48 +0800

    Optimize viewer

--- script/update-uml.sh
+++ script/update-uml.sh
@@ -4,4 +4,4 @@ puml_path=$2
 timestamp=$3
 update_js_path=$4
 java -Dapple.awt.UIElement=true -jar "$jar_path" "$puml_path" -tsvg
-echo window.updateDiagramURL('$timestamp') > "$update_js_path"
+echo "window.updateDiagramURL('$timestamp')" > "$update_js_path"
