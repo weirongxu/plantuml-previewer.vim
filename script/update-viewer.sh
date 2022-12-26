4dfe5d7ff814d8c6655876c5c1ec2e94c147aab8
Jumziey <jumziey@gmail.com>
2022-12-26 23:11:51 +0100

    Change linux scripts to skip writing to the temp scripts
    Initial test, will need further cleanup

--- script/update-viewer.sh
+++ script/update-viewer.sh
@@ -5,13 +5,12 @@ jar_path=$1
 puml_src_path=$2
 output_dir_path=$3
 output_path=$4
-finial_path=$5
+final_path=$5
 image_type=$6
 
 timestamp=$7
 update_js_path=$8
 include_path=$9
 
-java -Dapple.awt.UIElement=true -Djava.awt.headless=true -Dplantuml.include.path="$include_path" -jar "$jar_path" "$puml_src_path" -t$image_type -o "$output_dir_path"
-cp "$output_path" "$finial_path"
+java -Dapple.awt.UIElement=true -Djava.awt.headless=true -Dplantuml.include.path="$include_path" -jar "$jar_path" "$puml_src_path" -t$image_type -o "$final_path"
 echo "window.updateDiagramURL('$timestamp')" > "$update_js_path"
