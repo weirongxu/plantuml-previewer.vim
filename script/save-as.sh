192cbc2349d4a5c772089b4ea72e085fa61790ff
wanderer <mjbecze@riseup.net>
2023-02-07 04:43:22 +0100

    added variable java_path to specify the location of java (#85)
    Co-authored-by: Martin Becze <martin@becze.org>

--- script/save-as.sh
+++ script/save-as.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
-jar_path=$1
+java=$1
+jar_path=$2
 
-puml_src_path=$2
-output_dir_path=$3
-output_path=$4
-save_path=$5
-image_type=$6
-include_path=$7
+puml_src_path=$3
+output_dir_path=$4
+output_path=$5
+save_path=$6
+image_type=$7
+include_path=$8
 
-java -Dapple.awt.UIElement=true -Djava.awt.headless=true -Dplantuml.include.path="$include_path" -jar "$jar_path" "$puml_src_path" -t$image_type -o "$output_dir_path"
+$java -Dapple.awt.UIElement=true -Djava.awt.headless=true -Dplantuml.include.path="$include_path" -jar "$jar_path" "$puml_src_path" -t$image_type -o "$output_dir_path"
 cp "$output_path" "$save_path"
