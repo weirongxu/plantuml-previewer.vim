1dd4d0f2b09cd80a217f76d82f93830dbbe689b3
Weirong Xu <weirongxu.raidou@gmail.com>
2023-03-08 00:54:49 +0800

    fix: Support more arguments on scripts

--- script/save-as.sh
+++ script/save-as.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-java=$1
-jar_path=$2
+java=${1}
+jar_path=${2}
 
-puml_src_path=$3
-output_dir_path=$4
-output_path=$5
-save_path=$6
-image_type=$7
-include_path=$8
+puml_src_path=${3}
+output_dir_path=${4}
+output_path=${5}
+save_path=${6}
+image_type=${7}
+include_path=${8}
 
 $java -Dapple.awt.UIElement=true -Djava.awt.headless=true -Dplantuml.include.path="$include_path" -jar "$jar_path" "$puml_src_path" -t$image_type -o "$output_dir_path"
 cp "$output_path" "$save_path"
--- script/update-viewer.cmd
+++ script/update-viewer.cmd
@@ -1,15 +1,25 @@
 set java=%1
-set jar_path=%2
+shift
+set jar_path=%1
+shift
 
-set puml_src_path=%3
-set output_dir_path=%4
-set output_path=%5
-set finial_path=%6
-set image_type=%7
+set puml_src_path=%1
+shift
+set output_dir_path=%1
+shift
+set output_path=%1
+shift
+set finial_path=%1
+shift
+set image_type=%1
+shift
 
-set timestamp=%8
-set update_js_path=%9
-set include_path=%10
+set timestamp=%1
+shift
+set update_js_path=%1
+shift
+set include_path=%1
+shift
 
 %java% -Dapple.awt.UIElement=true -Dplantuml.include.path="%include_path%" -jar "%jar_path%" "%puml_src_path%" -t%image_type% -o "%output_dir_path%"
 echo F | xcopy /S /Q /F /Y  "%output_path%" "%finial_path%"
